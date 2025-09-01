45da8f2f62f5ee6813be76e543dd1ff8ef44e865
Dongseok Kim <jkds5920@gmail.com>
2025-09-01 12:19:05 +0900

    Create __init__.py

---
+++ src/gcfl/mechanisms/__init__.py
@@ -0,0 +1,12 @@
+"""
+Mechanism plugins for gcfl-sim.
+
+Importing this package registers built-in mechanisms into the global registry:
+- u_orth_penalty
+"""
+from __future__ import annotations
+
+# Import submodules to trigger @register_mechanism decorators
+from . import u_orth_penalty as _u_orth_penalty  # noqa: F401
+
+__all__ = ["_u_orth_penalty"]
